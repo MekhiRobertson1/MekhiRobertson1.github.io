bd6064ff7766337d10772975f4c4d905542da6e8
MekhiRobertson1 r <mekhi.robertson@sciacademy.org>
2023-09-14 15:05:35 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,6 +47,7 @@ $(function () {
     createPlatform(0, 200, 1300, 20);
     createPlatform(1300, 400, 1300, 20);
     createPlatform(300, 400, 1300, 20);
+    createPlatform(0, 600, 200, 20)
     // Actual Platforms //
     // Obstacles on First Platform //
     createPlatform(425, 150, 30, 50);
@@ -56,18 +57,25 @@ $(function () {
     createPlatform(750, 150, 30, 50);
     createPlatform(750, 0, 30, 50);
     createPlatform(925, 150, 30, 50);
+    createPlatform(925, 0, 30, 50);
     // Obstacles on First Platform //
     // Obstacles on Second Platform //
     createPlatform(300, 350, 30, 50);
+    createPlatform(500, 310, 50, 90);
+    createPlatform(700, 310, 50, 90);
+    createPlatform(900, 315, 50, 20);
+    createPlatform(1100, 310, 50, 90);
     // Obstacles on Second Platform //
-
+    // Obstacles on Third Platform //
+    createPlatform(300, 560, 100,20)
+    createPlatform(500, 580, 100,20)
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    createCollectable("steve", 500, 300, 20, 0.5);
+    createCollectable("steve", 910, 275, 20, 1);
 
 
 
@@ -76,11 +84,38 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("top", 400, 1000);
-    createCannon("bottom", 480, 1500);
-    createCannon("top", 750, 1000);
-    createCannon("bottom", 800, 1500);
-    createCannon("top", 1075, 1000);
+    createCannon("top", 400, 3050);
+    createCannon("top", 600, 3100);
+    createCannon("top", 750, 3150);
+    createCannon("top", 925, 3200);
+    createCannon("top", 1075, 3250);
+    createCannon("left", 240, 5000);
+    createCannon("left", 240, 5100);
+    createCannon("left", 240, 5200);
+    // Last Platform Cannons //
+    createCannon("left", 600, 1000);
+    createCannon("left", 610, 1050);
+    createCannon("left", 620, 1100);
+    createCannon("left", 630, 1150);
+    createCannon("left", 640, 1200);
+    createCannon("left", 650, 1250);
+    createCannon("left", 660, 1300);
+    createCannon("left", 670, 1350);
+    createCannon("left", 680, 1400);
+    createCannon("left", 690, 1450);
+    createCannon("left", 700, 1500);
+    createCannon("left", 600, 1600);
+    createCannon("left", 610, 1650);
+    createCannon("left", 620, 1700);
+    createCannon("left", 630, 1750);
+    createCannon("left", 640, 1800);
+    createCannon("left", 650, 1850);
+    createCannon("left", 660, 1900);
+    createCannon("left", 670, 1950);
+    createCannon("left", 680, 2000);
+    createCannon("left", 690, 2050);
+    createCannon("left", 700, 2100);
+    
     
 
 
