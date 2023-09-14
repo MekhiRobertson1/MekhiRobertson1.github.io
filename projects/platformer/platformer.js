3e6393ee21becc69571f1e728fc4aad4e67ea8d1
MekhiRobertson1 r <mekhi.robertson@sciacademy.org>
2023-09-14 15:12:07 +0000

    added platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -76,6 +76,7 @@ $(function () {
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
     createCollectable("steve", 910, 275, 20, 1);
+    
 
 
 
