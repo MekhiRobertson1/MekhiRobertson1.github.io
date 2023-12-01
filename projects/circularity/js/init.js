b72213cfec2e192326842b74085e22bb275f0f47
MekhiRobertson1 r <mekhi.robertson@sciacademy.org>
2023-12-01 15:05:51 +0000

    add bouncing box]

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,7 +32,9 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
+         
         drawCircle();
+        
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -45,18 +47,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(/* Your Bracket Notation HERE */);
-            physikz.updatePosition(/* Your Bracket Notation HERE */);
-            physikz.updatePosition(/* Your Bracket Notation HERE */);
-            physikz.updatePosition(/* Your Bracket Notation HERE */);
-            physikz.updatePosition(/* Your Bracket Notation HERE */);
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(/* Your Bracket Notation HERE */);
-            game.checkCirclePosition(/* Your Bracket Notation HERE */);
-            game.checkCirclePosition(/* Your Bracket Notation HERE */);
-            game.checkCirclePosition(/* Your Bracket Notation HERE */);
-            game.checkCirclePosition(/* Your Bracket Notation HERE */);
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
 
             // TODO 9 : Iterate over the array
            
@@ -73,10 +75,13 @@ var init = function (window) {
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
+        
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
