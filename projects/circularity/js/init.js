f953f5b04b7bd6f136fca61fd7e40f53e1d501b3
MekhiRobertson1 r <mekhi.robertson@sciacademy.org>
2023-12-04 16:28:54 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
             view.addChild(circle);
             circles.push(circle);
         }
@@ -51,7 +51,7 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
             
-            // Deleted because TODO 9 told me to, everything here is in TODO 9 //
+            
             
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
